339e435b051fc9c45f07f98c8bc3d488a561ec9f
vanessailana <vanessa.klotzman.17@my.csun.edu>
2018-12-10 20:12:19 -0800

    2nd deploy

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -8,6 +8,6 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'WNpe9mhpApNvI198mBLJwN1v8JwJVpHL',
   domain: 'letsshop.auth0.com',
-  callbackURL: 'http://localhost:4200/callback',
+  callbackURL: 'http://warm-tundra-14923.herokuapp.com/:4200/callback',
   apiUrl:'http://localhost:8083/api/'
 };
